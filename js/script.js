cabb94e192e3854adeb15a2d52ca552f54460593
sinchul <kscpqk66@gmail.com>
2023-06-26 18:17:03 +0900

    26일자 수정본

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
-/* function mainMenu(){
-    const menuTop=document.getElementsByClassName('m-menu')
-    const menuBottom=document.querySelectorAll('.category-list')
-    const menucategory=document.querySelector('.menu-category')
-    let menuLength=menuTop.length
-
-    for(let i=0;i<menuLength;i++){
-
-        menuTop[i].addEventListener('mouseover',function(){
-            for(let j=0;j<menuLength;j++){
-                menuBottom[j].classList.remove('active')
-            }
-            menuBottom[i].classList.add('active')
-        })
-        menucategory.addEventListener('mouseout',function(){
-
-            menuBottom[i].classList.remove('active');
-            
-        }) 
-    }
-}
-
- */
 function mainMenu() {
     const menuTop = document.getElementsByClassName('m-menu');
     const menuBottom = document.querySelectorAll('.category-list');
@@ -33,6 +10,7 @@ function mainMenu() {
                 menuBottom[j].classList.remove('active');
             }
             menuBottom[i].classList.add('active');
+            menucategory.classList.add('active')
         });
     }
 
@@ -40,6 +18,26 @@ function mainMenu() {
         for (let j = 0; j < menuLength; j++) {
             menuBottom[j].classList.remove('active');
         }
+        menucategory.classList.remove('active')
     });
 }
 mainMenu()
+
+function toggleMenu(){
+    const mobileBtn=document.querySelector('.desk-n .menu');
+    const mobileCBtn=document.querySelector('.mobile .m-close');
+    const mobileMenu=document.querySelector('.mobile');
+    const mobileBg=document.querySelector('.mobile-bg');
+    
+    mobileBtn.addEventListener('click',function(){
+        mobileBg.classList.add('active')
+        mobileMenu.classList.add('active')
+        /* mobileMenu.style.right=0; */
+    })
+    mobileCBtn.addEventListener('click',function(){
+        mobileBg.classList.remove('active')
+        mobileMenu.classList.remove('active')
+        /* mobileMenu.style.right=-270+'px'; */
+    })
+}
+toggleMenu()
