104f28e4e69c742aa94ded89363fc592ce3d7d7c
sinchul <kscpqk66@gmail.com>
2023-06-28 18:19:12 +0900

    완성

--- js/script.js
+++ js/script.js
@@ -131,3 +131,13 @@ function randomimage(){
     
 }
 randomimage()
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
